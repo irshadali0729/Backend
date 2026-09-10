8b4b4ec584df2e3bf7970e64b430db5c10611f3f
codeaffairs <codeaffairs729@gmail.com>
2026-09-10 06:18:51 +0530

    Backend first commit

---
+++ Server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  console.log("Home page api /");
+  res.status(201).send("<h3>Listing from Backend </h3>");
+});
+
+const profile={
+    name:"irshad",
+    add:"telco"
+}
+
+app.get("/profile", (req, res) => {
+  console.log("Home page api /");
+  res.status(201).json({
+    profile,
+  });
+});
+
+app.listen(8000, () => {
+  console.log("Server is listening on the port : ", 8000);
+});
